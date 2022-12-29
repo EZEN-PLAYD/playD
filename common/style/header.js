6c1258afbf56bf6d17e2592c74ad8ee88994b44e
“gowjdqkqh9” <gowjdqkqh9@naver.com>
2022-12-29 09:25:17 +0900

    header 수정

--- common/style/header.js
+++ common/style/header.js
@@ -1,121 +1,13 @@
+/********************** scroll event **********************/
 // header
-const creativeHeader = document.querySelector("#creativeHeader");
 const headerInner = document.querySelector("#headerInner");
 const logo = document.querySelector("#logo");
-
-// navigation
-const navList1 = document.querySelector(".nav_list.list1");
-const navList2 = document.querySelector(".nav_list.list2");
-const navList3 = document.querySelector(".nav_list.list3");
-const navList4 = document.querySelector(".nav_list.list4");
-
 // sub_menu
 const navmenu1 = document.querySelector("#navmenu1");
 const navmenu2 = document.querySelector("#navmenu2");
 const navmenu3 = document.querySelector("#navmenu3");
 const navmenu4 = document.querySelector("#navmenu4");
-const navmenuBg = document.querySelector("#navmenuBg");
 
-// toggle_button
-const toggleBtn = document.querySelector("#toggleBtn");
-const gnbBtn = document.querySelector("#gnbBtn");
-const cancel = document.querySelector("#cancel");
-
-// top_button
-const footer = document.querySelector("footer");
-const topBtn = document.querySelector("#topBtn");
-
-/********************** maouse event **********************/
-navList1.style.overflow = "hidden";
-navList2.style.overflow = "hidden";
-navList3.style.overflow = "hidden";
-navList4.style.overflow = "hidden";
-
-// list1
-navList1.addEventListener("mouseover", () => {
-  navmenuBg.style.height = "100px";
-  navmenuBg.style.borderTop = "1px solid #ddd";
-  navList1.style.removeProperty("overflow");
-
-  let i = headerInner.offsetTop;
-  if (scrollY > i) {
-    navmenuBg.style.height = "60px";
-    navmenuBg.style.top = "60px";
-  } else if (scrollY <= i) {
-    navmenuBg.style.height = "100px";
-    navmenuBg.style.top = "100px";
-  }
-});
-navList1.addEventListener("mouseout", () => {
-  navmenuBg.style.height = "0";
-  navmenuBg.style.borderTop = "none";
-  navList1.style.overflow = "hidden";
-});
-
-// list2
-navList2.addEventListener("mouseover", () => {
-  navmenuBg.style.height = "100px";
-  navmenuBg.style.borderTop = "1px solid #ddd";
-  navList2.style.removeProperty("overflow");
-
-  let i = headerInner.offsetTop;
-  if (scrollY > i) {
-    navmenuBg.style.height = "60px";
-    navmenuBg.style.top = "60px";
-  } else if (scrollY <= i) {
-    navmenuBg.style.height = "100px";
-    navmenuBg.style.top = "100px";
-  }
-});
-navList2.addEventListener("mouseout", () => {
-  navmenuBg.style.height = "0";
-  navmenuBg.style.borderTop = "none";
-  navList2.style.overflow = "hidden";
-});
-
-// list3
-navList3.addEventListener("mouseover", () => {
-  navmenuBg.style.height = "100px";
-  navmenuBg.style.borderTop = "1px solid #ddd";
-  navList3.style.removeProperty("overflow");
-
-  let i = headerInner.offsetTop;
-  if (scrollY > i) {
-    navmenuBg.style.height = "60px";
-    navmenuBg.style.top = "60px";
-  } else if (scrollY <= i) {
-    navmenuBg.style.height = "100px";
-    navmenuBg.style.top = "100px";
-  }
-});
-navList3.addEventListener("mouseout", () => {
-  navmenuBg.style.height = "0";
-  navmenuBg.style.borderTop = "none";
-  navList3.style.overflow = "hidden";
-});
-
-// list4
-navList4.addEventListener("mouseover", () => {
-  navmenuBg.style.height = "100px";
-  navmenuBg.style.borderTop = "1px solid #ddd";
-  navList4.style.removeProperty("overflow");
-
-  let i = headerInner.offsetTop;
-  if (scrollY > i) {
-    navmenuBg.style.height = "60px";
-    navmenuBg.style.top = "60px";
-  } else if (scrollY <= i) {
-    navmenuBg.style.height = "100px";
-    navmenuBg.style.top = "100px";
-  }
-});
-navList4.addEventListener("mouseout", () => {
-  navmenuBg.style.height = "0";
-  navmenuBg.style.borderTop = "none";
-  navList4.style.overflow = "hidden";
-});
-
-/********************** scroll event **********************/
 addEventListener("scroll", () => {
   let num = headerInner.offsetTop;
   if (scrollY > num) {
@@ -151,6 +43,10 @@ addEventListener("scroll", () => {
   }
 });
 
+// top_button
+const footer = document.querySelector("footer");
+const topBtn = document.querySelector("#topBtn");
+
 topBtn.style.display = "none";
 addEventListener("scroll", () => {
   let footerOffsetTop = footer.offsetTop;
@@ -164,6 +60,11 @@ addEventListener("scroll", () => {
 });
 
 /********************** click event **********************/
+// toggle_button
+const toggleBtn = document.querySelector("#toggleBtn");
+const gnbBtn = document.querySelector("#gnbBtn");
+const cancel = document.querySelector("#cancel");
+
 toggleBtn.addEventListener("click", (e) => {
   e.preventDefault();
   gnbBtn.classList.toggle("active");
